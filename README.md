2af3489bd7908150491c6f1c2f22ba4cb2e12d1d
Neto test <89373786148@yandex.ru>
2023-04-29 20:19:22 +0300

    second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/dpx5dluvcmc66new?svg=true)](https://ci.appveyor.com/project/VEVparashut/selenide)
--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,2 +1,34 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SelenideTest {
+    public static String setLocalDate(int day) {
+        String date = LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTestForDay3() {
+        $("[data-test-id=city] .input__control").setValue("Чебоксары");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] .input__control").setValue("+71234567890");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(3)));
+    }
 }
